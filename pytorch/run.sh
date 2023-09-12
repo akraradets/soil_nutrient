b07e9a6b6e29b4e44994634f19a1ade14c416942
Akraradet Sinsamersuk <akraradets@gmail.com>
2023-09-12 17:31:45 +0700

    fix bug in run script

--- pytorch/run.sh
+++ pytorch/run.sh
@@ -23,9 +23,9 @@ python3 linearplot.py   --image_set om --model_name alexnet --device samsung --e
 python3 train_bigset.py --image_set om --model_name alexnet --device redmi --environment outdoor   --clip_target True --normalize_target True --epochs 50 --lr 0.001
 python3 linearplot.py   --image_set om --model_name alexnet --device redmi --environment outdoor   --clip_target True --normalize_target True
 python3 train_bigset.py --image_set om --model_name alexnet --device oppo --environment outdoor   --clip_target True --normalize_target True --epochs 50 --lr 0.001
-python3 linearplot.py   --image_set om --model_name alexnet --device oppo --environment outdoor   --clip_target True --normalize_target True --epochs 50 --lr 0.001
+python3 linearplot.py   --image_set om --model_name alexnet --device oppo --environment outdoor   --clip_target True --normalize_target True
 
-python3 train_bigset.py --image_set p --model_name alexnet --device iphone --environment all  --clip_target True --normalize_target True
+python3 train_bigset.py --image_set p --model_name alexnet --device iphone --environment all  --clip_target True --normalize_target True --epochs 50 --lr 0.001
 python3 linearplot.py   --image_set p --model_name alexnet --device iphone --environment all  --clip_target True --normalize_target True
 python3 train_bigset.py --image_set p --model_name alexnet --device samsung --environment all --clip_target True --normalize_target True --epochs 50 --lr 0.001
 python3 linearplot.py   --image_set p --model_name alexnet --device samsung --environment all --clip_target True --normalize_target True
@@ -42,9 +42,9 @@ python3 linearplot.py   --image_set p --model_name alexnet --device iphone --env
 python3 train_bigset.py --image_set p --model_name alexnet --device samsung --environment outdoor --clip_target True --normalize_target True --epochs 50 --lr 0.001
 python3 linearplot.py   --image_set p --model_name alexnet --device samsung --environment outdoor --clip_target True --normalize_target True
 python3 train_bigset.py --image_set p --model_name alexnet --device redmi --environment outdoor   --clip_target True --normalize_target True --epochs 50 --lr 0.001
-python3 linearplot.py   --image_set p --model_name alexnet --device redmi --environment outdoor   --clip_target True --normalize_target True --epochs 50 --lr 0.001
+python3 linearplot.py   --image_set p --model_name alexnet --device redmi --environment outdoor   --clip_target True --normalize_target True
 
-# python3 train_bigset.py --image_set k --model_name alexnet --device iphone --environment all  --clip_target True --normalize_target True
+# python3 train_bigset.py --image_set k --model_name alexnet --device iphone --environment all  --clip_target True --normalize_target True --epochs 50 --lr 0.001
 # python3 linearplot.py   --image_set k --model_name alexnet --device iphone --environment all  --clip_target True --normalize_target True
 # python3 train_bigset.py --image_set k --model_name alexnet --device samsung --environment all --clip_target True --normalize_target True --epochs 50 --lr 0.001
 # python3 linearplot.py   --image_set k --model_name alexnet --device samsung --environment all --clip_target True --normalize_target True
